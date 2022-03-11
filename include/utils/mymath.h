0f6024d19aeaf2abc78d102b7f83d6219b8da634
UnnamedOrange <lycheng1215@sina.com>
2022-03-11 22:49:31 +0800

    Move comments

--- include/utils/mymath.h
+++ include/utils/mymath.h
@@ -4,5 +4,12 @@
 
 namespace App::utils::mymath
 {
+    /**
+     * @brief Calculate the sum of the two parameters.
+     *
+     * @param a1 One number.
+     * @param a2 Another number.
+     * @return int The sum of the two numbers.
+     */
     int add(int a1, int a2);
 } // namespace App::utils::mymath
