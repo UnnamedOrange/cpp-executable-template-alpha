911ae79cffb904248fd0f97e17da0bcc4efdc723
UnnamedOrange <lycheng1215@sina.com>
2022-03-15 22:43:14 +0800

    2022-3-15

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
-#include <fmt/format.h>
+#include "app_config.hpp"
+#include "utils/mymath.h"
 
-#include <app_config.hpp>
-#include <utils/mymath.h>
+#include <fmt/format.h>
+#include <fstream>
 
 using namespace app;
 using namespace app::utils::mymath;
@@ -10,6 +11,11 @@ int main()
 {
     fmt::print("This is {}, version {} (Build {}).\n", config::project_name,
                config::project_version, config::project_version_tweak);
+
+    std::ifstream ifs("content.txt");
+    std::string line;
+    std::getline(ifs, line);
+
     auto result = add(1, 2);
-    fmt::print("{0:03d}\n", result);
+    fmt::print("{0:03d}: {1}\n", result, line);
 }
